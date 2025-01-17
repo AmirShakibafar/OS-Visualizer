ae00dd6a6d2f11ccd7ae78c8b06959599b0c5126
Alireza Ahmadiniya <alireza00ahmadiniya@gmail.com>
2025-01-17 09:18:34 +0000

    TST & REF : Add HRRD.test.js & Reafctor FCFS and HRRN

--- js/process/FCFS.js
+++ js/process/FCFS.js
@@ -1,10 +1,5 @@
-import { sleep } from "../helpers/helpers.js";
-import {
-  isCancelled,
-  get_next_block,
-  SPEED,
-  ShowAvgTime,
-} from "./animation_table.js";
+import { Display } from "./display.js";
+import { ShowAvgTime } from "./animation_table.js";
 import { avgWaitTime } from "./avgWaitTimeCalculator.js"
 
 
@@ -24,40 +19,10 @@ const FCFSProcessSort =  (processes) =>{
 }
 
 
-const FCFSDisplay = async (processes) => {
-  let curr_tick = 0;
-  for (const process of processes) {
-    if (isCancelled) {
-      return;
-    }
-    while (process.start > curr_tick) {
-      if (isCancelled) {
-        return;
-      }
-      get_next_block(
-        { bgcolor: "#fcfcfc", color: "#000", name: "Idle" },
-        curr_tick
-      );
-      curr_tick++;
-      await sleep(SPEED);
-    }
-    let duration = process.duration;
-    while (duration > 0) {
-      if (isCancelled) {
-        return;
-      }
-      get_next_block(process, curr_tick);
-      duration--;
-      curr_tick++;
-      await sleep(SPEED);
-    }
-  }
-}
-
 const FCFS =  (processes) => {
   processes = FCFSProcessSort(processes);
   const AvgWaitTime = avgWaitTime(processes);
-  FCFSDisplay(processes);
+  Display(processes);
   ShowAvgTime(AvgWaitTime);
 };
 
