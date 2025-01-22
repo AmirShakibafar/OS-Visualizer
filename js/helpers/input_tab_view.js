4ad406d6105d102e655b56172e14ee793ca3cc51
Amir Shakibafar <amir.shakibafar82@gmail.com>
2025-01-22 17:30:51 +0330

    added new input section for desktop

---
+++ js/helpers/input_tab_view.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.tab').forEach(tab => {
+    tab.addEventListener('click', function() {
+      const targetPanel = this.getAttribute('data-target');
+
+      document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+      document.querySelectorAll('.tab-panel').forEach(panel => panel.classList.remove('active-body'));
+      
+      this.classList.add('active');
+      document.getElementById(targetPanel).classList.add('active-body');
+    });
+});
+  
