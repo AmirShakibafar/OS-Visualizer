f80dd09135adf3511e7b0c818e76d1c95e2e6b97
Amir Shakibafar <amir.shakibafar82@gmail.com>
2025-01-30 00:49:01 +0330

    added the basic logic to memory section

--- js/memory/animation_section.js
+++ js/memory/animation_section.js
@@ -9,22 +9,58 @@ import {
   readIsCancelled,
 } from "../helpers/cancelFlag.js";
 
-const playButton = document.getElementById("memory-play-button");
-const cancelButton = document.getElementById("memory-reset-button");
-const speedSlider = document.getElementById("speed-range");
+const playButton = document.getElementById("play-button");
+const resetButton = document.getElementById("reset-button");
+const speedBTN = document.getElementById("speedBTN");
 
-let SPEED = 1050 - speedSlider.value;
-const updateSPEED = () => {
-  SPEED = 1050 - speedSlider.value;
-  setAnimationSpeed(SPEED);
+const BASE_SPEED = 500;
+let stateSpeed = 1;
+let speed = BASE_SPEED ;
+
+speedBTN.addEventListener("click", (event) => {
+  switch (stateSpeed){
+    case 0:
+      event.target.textContent = "0.5X"
+      speed = BASE_SPEED / 0.5 ;
+      stateSpeed++;
+      break ;
+    case 1:
+      event.target.textContent = "1X"
+      speed = BASE_SPEED ;
+      stateSpeed++;
+      break ;
+    case 2:
+      event.target.textContent = "2X"
+      speed = BASE_SPEED / 2 ;
+      stateSpeed++;
+      break ;
+    case 3:
+      event.target.textContent = "4X"
+      speed = BASE_SPEED / 4;
+      stateSpeed++;
+      break ;
+    case 4:
+      event.target.textContent = "8X"
+      speed = BASE_SPEED / 8;
+      stateSpeed++;
+      break ;
+    case 5:
+      event.target.textContent = "16X"
+      speed = BASE_SPEED / 16;
+      stateSpeed = 0;
+      break ;
+  }
+  updateSpeed();
+})
+
+
+const updateSpeed = () => {
+  setAnimationSpeed(speed);
 };
-updateSPEED();
-speedSlider.addEventListener("input", updateSPEED);
 
 const resetAnimationSection = () => {
   clearMemorySpaces();
   renderMemorySections();
-  resetTime();
 };
 
 const playHandler = async () => {
@@ -40,10 +76,10 @@ const playHandler = async () => {
   playButton.disabled = false;
 };
 
-const cancelButtonHandler = () => {
+const resetButtonHandler = () => {
   setIsCancelled();
   resetAnimationSection();
 };
 
 playButton.addEventListener("click", playHandler);
-cancelButton.addEventListener("click", cancelButtonHandler);
+resetButton.addEventListener("click", resetButtonHandler);
